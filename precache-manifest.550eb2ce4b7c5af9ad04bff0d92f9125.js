fd9db8759190019d4a7c631979a17d5278316f23
Cherrymountains <38588115+Cherrymountains@users.noreply.github.com>
2020-09-12 12:06:33 +0200

    Updates

---
+++ precache-manifest.550eb2ce4b7c5af9ad04bff0d92f9125.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a03371c3830c6787a76221e3497b5ba",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "542f9648a77a70d85a4e",
+    "url": "/monsters-rolodex/static/css/main.cd615b70.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542f9648a77a70d85a4e",
+    "url": "/monsters-rolodex/static/js/main.6e095853.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
